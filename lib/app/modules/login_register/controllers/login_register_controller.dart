8762febf2196a6975212519599393a6f0017050a
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-07-28 17:04:44 +0600

    login and register page UI 50% done

---
+++ lib/app/modules/login_register/controllers/login_register_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+
+class LoginRegisterController extends GetxController {
+
+  var mailOrPhoneController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  final dropdownValue = 'Admin'.obs;
+  final playRoleID = ''.obs;
+  final playRoleName = ''.obs;
+
+
+
+
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+
+  createUserAccount() async {
+
+  }
+
+  userLogin(){}
+}
---
+++ lib/app/modules/login_register/views/login_register_view.dart
@@ -0,0 +1,85 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../../../core/theme/style_text.dart';
+import '../../../core/values/app_colors.dart';
+import '../../../core/values/app_space.dart';
+import '../../../global_widgets/custom_text_field.dart';
+import '../../../helpers/app_helper.dart';
+import '../controllers/login_register_controller.dart';
+
+class LoginRegisterView extends GetView<LoginRegisterView> {
+  const LoginRegisterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child:
+      Scaffold(backgroundColor: AppColors.white, body: _buildBody(context)),
+    );
+  }
+
+  _buildBody(BuildContext context) {
+    return GetBuilder<LoginRegisterController>(
+      builder: (_) {
+        return Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    width: 200,
+                    height: 200,
+                  ),
+                  AppSpace.spaceH14,
+                  CustomTextField(
+                    controller: _.mailOrPhoneController,
+                    padding: const EdgeInsets.only(right: 10, left: 10),
+                    decoration: Helpers.customCircularDecoretion(
+                        context, AppColors.grey.withOpacity(0.2), 10),
+                    hintText: "EnterEmail".tr,
+                    style: AppTextStyle.labelSmall,
+                  ),
+                  AppSpace.spaceH14,
+                  CustomTextField(
+                    controller: _.passwordController,
+                    padding: const EdgeInsets.only(right: 10, left: 10),
+                    decoration: Helpers.customCircularDecoretion(
+                        context, AppColors.grey.withOpacity(0.2), 10),
+                    hintText: "EnterPassword".tr,
+                    style: AppTextStyle.labelSmall,
+                  ),
+                  AppSpace.spaceH26,
+                  InkWell(
+                    onTap: () {
+
+                    },
+                    child: Container(
+                      height: 50,
+                      width: Get.width,
+                      alignment: Alignment.center,
+                      decoration: Helpers.customCircularDecoretion(
+                          context, AppColors.primaryColor, 10),
+                      child: Text(
+                        'Register ',
+                        style: AppTextStyle.button,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+}
+
