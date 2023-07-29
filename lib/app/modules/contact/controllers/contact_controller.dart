149aff082c481c490366347d6d6ca8e989b129ff
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-07-29 23:24:42 +0600

    message sent api working

--- lib/app/modules/contact/controllers/contact_controller.dart
+++ lib/app/modules/contact/controllers/contact_controller.dart
@@ -28,6 +28,7 @@ class ContactController extends GetxController {
     super.onClose();
   }
 
+  
   getContactList()async{
    var res = await repo.getContactList();
    var result=jsonDecode(res.body);
